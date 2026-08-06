6c52bc427bad8acd6229b3d3ef7736fb4154264d
danhdct122c3 <144209033+danhdct122c3@users.noreply.github.com>
2026-08-06 17:51:18 +0700

    fix(frontend): import FaceLiveness CSS to fix component layout and scanning

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
+import '@aws-amplify/ui-react-liveness/styles.css';
 import App from './App.jsx'
 import './index.css'
 
